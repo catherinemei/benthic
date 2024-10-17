afb183318add8d9f23c1a3573f3041f85cf9456b
Catherine Mei <meic1212@mit.edu>
2024-10-17 11:39:53 -0400

    new directions

--- src/App.tsx
+++ src/App.tsx
@@ -23,12 +23,14 @@ const App: Component = () => {
 
       <h1>Commands</h1>
       <ul>
-        <li>
-          Press Left/Right/Up/Down arrows for traverse nodes in same layer
-        </li>
-        <li>Press Shift + Up Arrow to go to parent layer</li>
-        <li>Press Shift + Down Arrow to go to first child</li>
+        <li>Press Left/Right/Up/Down arrows to traverse nodes in same layer</li>
+        <li>Press Shift + Up Arrow to navigate to parent/ parent layer</li>
+        <li>Press Shift + Down Arrow to navigate to first child</li>
         <li>Press Enter to select node</li>
+        <li>Press H to refocus on current node (in adjacency layer) </li>
+        <li>
+          [Parent Focus only] Press P to switch parent in focus / parent context
+        </li>
       </ul>
     </div>
   );
