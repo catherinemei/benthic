4b33fc5e99976a60a69a8b0d0dce2d7ca28a2217
Catherine Mei <meic1212@mit.edu>
2024-10-17 01:39:32 -0400

    updated code and data

--- src/App.tsx
+++ src/App.tsx
@@ -6,12 +6,29 @@ const App: Component = () => {
       <h1>Home page</h1>
       <p>Select a prototype to view:</p>
       <a href="https://catherinemei.github.io/benthic-proto-one/">
-        Go to Prototype 1 - Parent Focus
+        Go to Prototype 1 - Parent Focus with Data Vis
       </a>
       <br />
       <a href="https://catherinemei.github.io/benthic-proto-two/">
-        Go to Prototype 2 - Flat
+        Go to Prototype 2 - Flat with Data Vis
       </a>
+      <a href="https://catherinemei.github.io/benthic-proto-three/">
+        Go to Prototype 1 - Parent Focus with Chemistry
+      </a>
+      <br />
+      <a href="https://catherinemei.github.io/benthic-proto-four/">
+        Go to Prototype 2 - Flat with Chemistry
+      </a>
+
+      <h1>Commands</h1>
+      <ul>
+        <li>
+          Press Left/Right/Up/Down arrows for traverse nodes in same layer
+        </li>
+        <li>Press Shift + Up Arrow to go to parent layer</li>
+        <li>Press Shift + Down Arrow to go to first child</li>
+        <li>Press Enter to select node</li>
+      </ul>
     </div>
   );
 };
